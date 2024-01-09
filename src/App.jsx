a76b353b48df62a2b16d0bf2be3a5a970af7976f
divyamprabhudessai <144110931+divyamprabhudessai@users.noreply.github.com>
2024-01-09 10:44:36 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -30,17 +30,16 @@ function App() {
   console.log(images);
 
   return(
-    <div >
-      <h1 className='title'>KALVIUM  GALLERY</h1>
-    
+    <div>
+      <h1>KALVIUM GALLERY</h1>
     <div className='grid'>
       {images.map((element ) => (
       <img src={element.img} id={element.id} width={"650vw"} />
     ))};
     </div>
-    
 
     </div>
+
 
 
   )
